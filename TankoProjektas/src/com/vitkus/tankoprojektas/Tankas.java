0819ce55bc728f51737109a60ceddde17fdaf84e
mvitkus <vitkusmindaugas@gmail.com>
2019-06-11 14:35:02 +0300

    tikėkimės final

--- TankoProjektas/src/com/vitkus/tankoprojektas/Tankas.java
+++ TankoProjektas/src/com/vitkus/tankoprojektas/Tankas.java
@@ -3,12 +3,12 @@ package com.vitkus.tankoprojektas;
 public class Tankas {
 
 	private Point point = new Point(0, 0);
-	
-	private int šūvisĮpriekį =0;
-	private int šūvisAtgal =0;
-	private int šūvisĮKairę =0;
-	private int šūvisĮDešinę =0;
-	
+
+	private int šūvisĮpriekį = 0;
+	private int šūvisAtgal = 0;
+	private int šūvisĮKairę = 0;
+	private int šūvisĮDešinę = 0;
+
 	private int getŠūvisĮpriekį() {
 		return šūvisĮpriekį;
 	}
@@ -41,8 +41,6 @@ public class Tankas {
 		this.šūvisĮDešinę = šūvisĮDešinę;
 	}
 
-
-
 	private Kryptys getKryptis() {
 		return kryptis;
 	}
@@ -61,7 +59,6 @@ public class Tankas {
 //		this.point = point;
 //	}
 
-
 	public void Pirmyn() {
 
 		int x = point.getX();
@@ -73,7 +70,7 @@ public class Tankas {
 		point.setY(y);
 
 		setKryptis(Kryptys.PIRMYN);
-		
+
 		System.out.println("Tankas pajuda į Šiaurę " + point);
 
 	}
@@ -89,7 +86,7 @@ public class Tankas {
 		point.setY(y);
 
 		setKryptis(Kryptys.ATGAL);
-		
+
 		System.out.println("Tankas pajuda į Pietus " + point);
 
 	}
@@ -105,7 +102,7 @@ public class Tankas {
 		point.setY(y);
 
 		setKryptis(Kryptys.DEŠINĖ);
-		
+
 		System.out.println("Tankas pajuda į Rytus " + point);
 
 	}
@@ -121,72 +118,65 @@ public class Tankas {
 		point.setY(y);
 
 		setKryptis(Kryptys.KAIRĖ);
-		
+
 		System.out.println("Tankas pajuda į Vakarus " + point);
 
 	}
 
 	public void Suvis() {
-		
-		if(getKryptis() == Kryptys.PIRMYN) {
-			setŠūvisĮpriekį(šūvisĮpriekį+1);
+
+		if (getKryptis() == Kryptys.PIRMYN) {
+			setŠūvisĮpriekį(šūvisĮpriekį + 1);
 			System.out.println("Šūvis į Šiaurę");
-		}else if(getKryptis() == Kryptys.ATGAL) {
-			setŠūvisAtgal(šūvisAtgal+1);
+		} else if (getKryptis() == Kryptys.ATGAL) {
+			setŠūvisAtgal(šūvisAtgal + 1);
 			System.out.println("Šūvis į Pietus");
-		}else if(getKryptis() == Kryptys.DEŠINĖ) {
-			setŠūvisĮDešinę(šūvisĮDešinę+1);
+		} else if (getKryptis() == Kryptys.DEŠINĖ) {
+			setŠūvisĮDešinę(šūvisĮDešinę + 1);
 			System.out.println("Šūvis į Rytus");
-		}else {setŠūvisĮKairę(šūvisĮKairę+1);
-		System.out.println("Šūvis į Vakarus");}
+		} else {
+			setŠūvisĮKairę(šūvisĮKairę + 1);
+			System.out.println("Šūvis į Vakarus");
+		}
 	}
 
 	public void Info() {
+
 		
-		System.out.println();
-		System.out.print("INFO: Tanko koordinatės: "+ point);
-		
-		if(getKryptis()== Kryptys.PIRMYN) {
-			System.out.println(", kryptis: VAKARAI ");
+		System.out.print("INFO: Tanko koordinatės: " + point);
+
+		if (getKryptis() == Kryptys.PIRMYN) {
+			System.out.println(", kryptis: ŠIAURĖ ");
 		}
-			
-		if(getKryptis()== Kryptys.ATGAL) {
+
+		if (getKryptis() == Kryptys.ATGAL) {
 			System.out.println(", kryptis: PIETŪS ");
 		}
-		
-		if(getKryptis()== Kryptys.KAIRĖ) {
+
+		if (getKryptis() == Kryptys.KAIRĖ) {
 			System.out.println(", kryptis: VAKARAI ");
 		}
-		
-		if(getKryptis()== Kryptys.DEŠINĖ) {
+
+		if (getKryptis() == Kryptys.DEŠINĖ) {
 			System.out.println(", kryptis: RYTAI ");
 		}
-				
+
 		
-		System.out.println();
-		System.out.print("INFO: Tanko šūviai: " );
+		System.out.print("INFO: Tanko šūviai: ");
+
+		System.out.print(getŠūvisĮpriekį() + " į Šiaurę, ");
+		System.out.print(getŠūvisĮDešinę() + " į Rytus, ");
+
+		System.out.print(getŠūvisAtgal() + " į Pietus, ");
+		System.out.print(getŠūvisĮKairę() + " į Vakarus, ");
+
+		int suviaiTotal = getŠūvisAtgal() + getŠūvisĮDešinę() + getŠūvisĮKairę() + getŠūvisĮpriekį();
+
+		System.out.println("Iš viso šūvių: " + suviaiTotal);
+
 		
-		System.out.print(getŠūvisĮpriekį() + " į Šiaurę, " );
-		System.out.print(getŠūvisĮDešinę()+ " į Rytus, ");
-		
-		System.out.print(getŠūvisAtgal()+ " į Pietus, " );
-		System.out.print(getŠūvisĮKairę()+ " į Vakarus, ");
-		
-		 int suviaiTotal = getŠūvisAtgal()+getŠūvisĮDešinę()+getŠūvisĮKairę()+getŠūvisĮpriekį();
-		
-		System.out.println("Iš viso šūvių: "+ suviaiTotal);
-		
-		
-			
-		
-		
-		System.out.println();
 
 	}
-	
-	private int SuviaiIsViso() {
-		
-		return getŠūvisĮpriekį()+getŠūvisAtgal()+getŠūvisĮDešinę()+getŠūvisĮKairę();
-	}
+
 
 }
