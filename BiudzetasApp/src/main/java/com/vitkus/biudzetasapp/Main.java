1bf97a6b2fff9f18d10c40c5a5d0677d051338c9
mvitkus <vitkusmindaugas@gmail.com>
2019-06-12 10:32:47 +0300

    meniu pradzia

--- BiudzetasApp/src/main/java/com/vitkus/biudzetasapp/Main.java
+++ BiudzetasApp/src/main/java/com/vitkus/biudzetasapp/Main.java
@@ -1,18 +1,59 @@
 package com.vitkus.biudzetasapp;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Biudzetas b1 = new Biudzetas();
-		b1.pridetiIslaiduIrasa(300.00, true, "Uz nieka");
-		b1.pridetiIslaiduIrasa(500.00, false, "siaip");
-		b1.pridetiPajamuIrasa(300.00, true, "uz ledus");
-		b1.pridetiPajamuIrasa(500.00, false, "uz pietus");
-		b1.pridetiPajamuIrasa(800.00, true, "uz pietus");
-		b1.pridetiPajamuIrasa(800.50, false, "uz dovana");
-		b1.gautiPajamuIrasa();
-		b1.gautiIslaiduIrasa();
+
+		Scanner sc = new Scanner(System.in);
+		Biudzetas biudzetas = new Biudzetas();
+		String input;
+		boolean runProgram = true;
+		Meniu meniu = new Meniu();
 		
+
+		while (runProgram) {
+			meniu.printMeniu();
+			input = sc.next();
+
+			if (input.equals("p")) {
+				
+				System.out.println("Iveskite pajamas: suma, ar gauta i banka (taip, ne), paskirtis");
+				Double suma = sc.nextDouble();
+				String rusis = sc.next();
+				String paskirtis = sc.next();
+				boolean tipas;
+		
+				if(rusis == "taip") {
+					tipas = true;
+				}else {tipas = false;}
+				
+				biudzetas.pridetiPajamuIrasa(suma, tipas, paskirtis);
+				
+			}
+			if (input.equals("i")) {
+				biudzetas.gautiPajamuIrasa();
+
+			}
+			if (input.equals("x")) {
+				runProgram=false;
+				break;
+			}
+
+		}
+
+//		Biudzetas b1 = new Biudzetas();
+//		b1.pridetiIslaiduIrasa(300.00, true, "Uz nieka");
+//		b1.pridetiIslaiduIrasa(500.00, false, "siaip");
+//		b1.pridetiPajamuIrasa(300.00, true, "uz ledus");
+//		b1.pridetiPajamuIrasa(500.00, false, "uz pietus");
+//		b1.pridetiPajamuIrasa(800.00, true, "uz pietus");
+//		b1.pridetiPajamuIrasa(800.50, false, "uz dovana");
+//		b1.gautiPajamuIrasa();
+//		b1.gautiIslaiduIrasa();
+//		System.out.println();
+
 	}
 
 }
---
+++ BiudzetasApp/src/main/java/com/vitkus/biudzetasapp/Meniu.java
@@ -0,0 +1,12 @@
+package com.vitkus.biudzetasapp;
+
+public class Meniu {
+	public void printMeniu() {
+		System.out.println("Pasirinkite: ");
+		System.out.println("[p] - Pajamu ivedimas");
+		System.out.println("[i] - Islaidu ivedimas");
+		System.out.println("[x] - Uzbaigti programa");
+
+	}
+
+}
